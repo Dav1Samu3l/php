74f1643287e8010fef53882133ff9c81d0e11fc4
dav1Samu3l <davidev.front@gmail.com>
2024-08-06 16:08:10 -0300

    introducao ao PHP

---
+++ script.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet">
+    <title></title>
+</head>
+
+<body>
+    <h1>Fundamentos do exercicio 01 </h1>
+    <?php
+    $nome=$_POST['name'];
+    
+
+
+
+
+    // $nome=$_POST['nome'];
+    // $nota1=$_POST['nota1'];
+    // $nota2=$_POST['nota2'];
+    // $peso1=$_POST['peso1'];
+    // $peso2=$_POST['peso2'];
+    // $media=($nota1*$peso1+$nota2*$peso2)/($peso1+$peso2);
+    // $media=number_format($media,2);
+    // $media=round($media,2);
+    // $media=number_format($media,2);
+    // $media=round($media,2);
+    // $media=number_format($media,2);
+    // $media=round($media,2);
+    // echo "A media do $nome e $media";
+
+
+
+
+
+
+
+
+    ?>
+
+
+
+</body>
+
+</html>
